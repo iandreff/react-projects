0c3e78ee89fa3c11e337acba92edf1546a72562b
ygor_gmetrix <ygor@gmetrix.net>
2018-09-28 18:34:29 -0600

    Initial commit

---
+++ vidly/src/components/movies.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { getMovies } from "../Services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies()
+  };
+
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(elem => elem._id !== movie._id);
+    this.setState({ movies: movies }); //this is equal to {movies}
+  };
+
+  render() {
+    const { length: count } = this.state.movies;
+    if (count === 0) return <p>There are no movies in DB</p>;
+
+    return (
+      <React.Fragment>
+        <p>Showing {count} movies in the DB</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;
