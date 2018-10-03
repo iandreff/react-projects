87809b6921136d2dece0d8a162081acf0c0f88b9
iandreff <36855238+iandreff@users.noreply.github.com>
2018-10-02 18:20:35 -0600

    added sevaral features
    Co-Authored-By: iandreff <iandreff@users.noreply.github.com>

--- counter-app/src/components/Counter.jsx
+++ counter-app/src/components/Counter.jsx
@@ -4,7 +4,7 @@ class Counter extends Component {
   render() {
     return (
       <div className="row">
-        <div className="col-1">
+        <div className="col-1 m-1">
           <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         </div>
         <div className="col">
@@ -17,12 +17,13 @@ class Counter extends Component {
           <button
             onClick={() => this.props.onDecrement(this.props.counter)}
             className="btn btn-secondary btn-sm m-2"
+            disabled={this.props.counter.value === 0 ? "disabled" : ""}
           >
             -
           </button>
           <button
             onClick={() => this.props.onDelete(this.props.counter.id)}
-            className="btn btn-danger btn-sm m-2"
+            className="btn btn-danger btn-sm"
           >
             X
           </button>
